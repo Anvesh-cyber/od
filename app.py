10909c5665572df9b63414c00f1a49cdba0f966d
Anvesh-cyber <98794076+Anvesh-cyber@users.noreply.github.com>
2022-12-14 14:31:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ AWS_SECRET_ACCESS = 'AdxVdsOcgoxXib77n+NjnyqRRPm4eMi4udai9s/1'
 
 app = Flask(__name__)
 
-CORS(app)
+# CORS(app)
 # Helper functions
 
 
@@ -56,63 +56,63 @@ def index():
 #     )
 
 
-@app.route("/predict")
-def predict():
-    try:
-        try:
-            shutil.rmtree("./images/labeled")
-        except:
-            pass
-        req = request.get_json()
-        imageName = req["image"]
-        filename = "./images/rawImages/" + imageName
+# @app.route("/predict")
+# def predict():
+#     try:
+#         try:
+#             shutil.rmtree("./images/labeled")
+#         except:
+#             pass
+#         req = request.get_json()
+#         imageName = req["image"]
+#         filename = "./images/rawImages/" + imageName
 
-        my_config = Config(region_name='ap-south-1')
-        # Let's use Amazon S3
-        s3 = boto3.resource("s3", config=my_config, aws_access_key_id=AWS_ACCESS_KEY,
-                            aws_secret_access_key=AWS_SECRET_ACCESS)
-        # Print out bucket names
-        s3 = boto3.client("s3", config=my_config, aws_access_key_id=AWS_ACCESS_KEY,
-                          aws_secret_access_key=AWS_SECRET_ACCESS)
-        s3.download_file(Bucket="property-images-bucket", Key=imageName, Filename=filename)
-
-
-
-
-        image_obj = Image.open(filename)
-
-        model = torch.hub.load('ultralytics/yolov5', 'custom', './model/best.pt', 'local')  # local repo
-        model.conf = 0.5
-        results = model(image_obj)
-        detect_res = results.pandas().xyxy[0].to_json(orient="records")
-        detect_res = json.loads(detect_res)
-        results.save(save_dir="./images/labeled")
-
-        labeled_filename = "./images/labeled/" + imageName
-
-        my_config = Config(region_name='ap-south-1')
-        # Let's use Amazon S3
-        s3 = boto3.resource("s3", config=my_config, aws_access_key_id=AWS_ACCESS_KEY,
-                            aws_secret_access_key=AWS_SECRET_ACCESS)
-        # Print out bucket names
-        s3 = boto3.client("s3", config=my_config, aws_access_key_id=AWS_ACCESS_KEY,
-                          aws_secret_access_key=AWS_SECRET_ACCESS)
-        s3.upload_file(
-            Filename=labeled_filename,
-            Bucket="property-images-detection-bucket",
-            Key=imageName
-        )
+#         my_config = Config(region_name='ap-south-1')
+#         # Let's use Amazon S3
+#         s3 = boto3.resource("s3", config=my_config, aws_access_key_id=AWS_ACCESS_KEY,
+#                             aws_secret_access_key=AWS_SECRET_ACCESS)
+#         # Print out bucket names
+#         s3 = boto3.client("s3", config=my_config, aws_access_key_id=AWS_ACCESS_KEY,
+#                           aws_secret_access_key=AWS_SECRET_ACCESS)
+#         s3.download_file(Bucket="property-images-bucket", Key=imageName, Filename=filename)
 
 
 
 
+#         image_obj = Image.open(filename)
+
+#         model = torch.hub.load('ultralytics/yolov5', 'custom', './model/best.pt', 'local')  # local repo
+#         model.conf = 0.5
+#         results = model(image_obj)
+#         detect_res = results.pandas().xyxy[0].to_json(orient="records")
+#         detect_res = json.loads(detect_res)
+#         results.save(save_dir="./images/labeled")
+
+#         labeled_filename = "./images/labeled/" + imageName
+
+#         my_config = Config(region_name='ap-south-1')
+#         # Let's use Amazon S3
+#         s3 = boto3.resource("s3", config=my_config, aws_access_key_id=AWS_ACCESS_KEY,
+#                             aws_secret_access_key=AWS_SECRET_ACCESS)
+#         # Print out bucket names
+#         s3 = boto3.client("s3", config=my_config, aws_access_key_id=AWS_ACCESS_KEY,
+#                           aws_secret_access_key=AWS_SECRET_ACCESS)
+#         s3.upload_file(
+#             Filename=labeled_filename,
+#             Bucket="property-images-detection-bucket",
+#             Key=imageName
+#         )
 
 
-        return {"predict": detect_res, "filename": imageName, "completed": True}
-
-    except:
-        return {"predict": [], "filename": "", "completed": False, "message": "Unable to predict the given image"}
 
 
-if __name__ == '__app__':
+
+
+#         return {"predict": detect_res, "filename": imageName, "completed": True}
+
+#     except:
+#         return {"predict": [], "filename": "", "completed": False, "message": "Unable to predict the given image"}
+
+
+if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=False)
